8fe260bf3e1032509eca55f8e041e7d04b573245
Alex collado <Alexanderyt777@gmail.com>
2023-07-24 18:27:24 -0400

    Initial commit

---
+++ src/main/java/com/group6/gym/app/AtlGymProjectApplication.java
@@ -0,0 +1,13 @@
+package com.group6.gym.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlGymProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlGymProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/gym/app/AtlGymProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.gym.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlGymProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
